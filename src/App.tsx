4f4186e26f3b5cd927ce4cc4c08a6737470c9de7
Grant Liu <grant.liux@gmail.com>
2022-07-25 17:35:00 +1200

    set up api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// Declare a new state variable, which we'll call "cityName"
+	const [cityName, setCityName] = useState("");
+
+	return (
+		<div>
+			<h1>
+				Weather app
+			</h1>
+
+			<div>
+				<label>City Name</label><br />
+				<input type="text" id="city-name" name="city-name" onChange={e => setCityName(e.target.value)} /><br />
+				<button onClick={search}>
+					Search
+				</button>
+			</div>
+
+			<p>
+				You have entered {cityName}
+			</p>
+		</div>
+	);
+
+	function search() {
+		axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=3a7040e91d9af7048ae6f18454e40e0c&units=metric`).then((res) => {
+			console.log(res.data);
+
+		})
+	}
 }
 
 export default App;
